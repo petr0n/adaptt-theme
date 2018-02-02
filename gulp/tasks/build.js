6e37fbd12813ed777fc600e201ca51c8ded4324c
Peter Abeln <pabeln@webershandwick.com>
2018-02-02 09:01:26 -0600

    first commit

---
+++ gulp/tasks/build.js
@@ -0,0 +1,5 @@
+var gulp    = require('gulp');
+var changed = require('gulp-changed');
+
+//gulp.task('build', ['browserify', 'styles', 'images', 'admin']);
+gulp.task('build', ['browserify', 'styles']);
